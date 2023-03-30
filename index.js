67db3ec602db131453c0cf0bf3a53b9654fa08b9
GreenRoot <1.ooo.ooo@bk.ru>
2023-03-30 18:20:20 +0300

    Added fav icons

---
+++ index.js
@@ -0,0 +1,8 @@
+
+
+
+gsap.fromTo("#title1", {x: -300, opacity: 0}, {duration: .5, x: 0, opacity: 1, delay: 1})
+gsap.fromTo("#title2", {x: -300, opacity: 0}, {duration: .7, x: 0, opacity: 1, delay: 1.3})
+gsap.fromTo("#title3", {x: -300, opacity: 0}, {duration: .9, x: 0, opacity: 1, delay: 1.6})
+gsap.fromTo("#title4", {x: -300, opacity: 0}, {duration: .9, x: 0, opacity: 1, delay: 2})
+
